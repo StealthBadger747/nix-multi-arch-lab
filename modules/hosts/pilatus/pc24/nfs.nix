5aa64e83ee434b53a2305a8d6b18acbb13f50586
Erik Parawell <parawell.erik@gmail.com>
2025-05-29 18:40:50 -0700

    Changed NFS rules

--- modules/hosts/pilatus/pc24/nfs.nix
+++ modules/hosts/pilatus/pc24/nfs.nix
@@ -1,12 +1,14 @@
 { config, lib, pkgs, ... }:
 {
   fileSystems."/export/share-stash" = {
-    device = "/BIGBOY/stash";
+    device = "/BIGBOY/JBOD/stash";
     options = [ "bind" ];
   };
 
+  networking.firewall.allowedTCPPorts = [ 2049 ];
+
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /export/share-stash  10.16.0.30(rw,insecure,no_subtree_check)
+   /export/share-stash  10.16.0.30(rw,insecure,no_subtree_check) 100.64.0.20(rw,insecure,no_subtree_check)
   '';
 }
