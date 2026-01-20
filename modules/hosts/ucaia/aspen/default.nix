7e3220f14b994b6c8b82a33c4388bff7a4346369
Erik Parawell <parawell.erik@gmail.com>
2026-01-19 16:23:57 -0800

    Fix conflict and add the rest of the hosts

--- modules/hosts/ucaia/aspen/default.nix
+++ modules/hosts/ucaia/aspen/default.nix
@@ -27,25 +27,7 @@ in {
     ./proxmox-settings.nix
   ];
 
-  nix.settings = {
-    substituters = [
-      "https://cache.nixos.org?priority=1"
-      "https://nix-community.cachix.org?priority=2"
-      "https://cuda-maintainers.cachix.org?priority=3"
-      "https://numtide.cachix.org?priority=4"
-      "https://cache.flox.dev?priority=5"
-      "https://deploy-rs.cachix.org?priority=6"
-    ];
 
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
-      "deploy-rs.cachix.org-1:xfNobmiwF/vzvK1gpfediPwpdIP0rpDV2rYqx40zdSI="
-    ];
-  };
 
   environment.systemPackages = with pkgs; [
     alejandra
@@ -146,6 +128,22 @@ in {
     };
 
     settings = {
+      substituters = [
+        "https://cache.nixos.org?priority=1"
+        "https://nix-community.cachix.org?priority=2"
+        "https://cuda-maintainers.cachix.org?priority=3"
+        "https://numtide.cachix.org?priority=4"
+        "https://deploy-rs.cachix.org?priority=6"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+        "deploy-rs.cachix.org-1:xfNobmiwF/vzvK1gpfediPwpdIP0rpDV2rYqx40zdSI="
+      ];
+
       auto-optimise-store = true;
       trusted-users = [ "root" "erikp" "connorgolden" ];
       experimental-features = [ "flakes" "nix-command" ];
