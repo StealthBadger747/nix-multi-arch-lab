bb18609ccaacba94b68e867257cc795806788506
StealthBadger747 <ac130kire@gmail.com>
2026-09-22 23:53:17 -0700

    oci-authentik: proxy MX ACME challenges

--- modules/hosts/oracle-cloud/free-aarch64.nix
+++ modules/hosts/oracle-cloud/free-aarch64.nix
@@ -199,6 +199,18 @@ in {
           '';
         };
       };
+      # Forward Stalwart's HTTP-01 challenge for the public MX hostname over
+      # the private tailnet. TLS terminates at Stalwart for SMTP itself.
+      virtualHosts."mx.ucaia.com" = {
+        locations."/" = {
+          proxyPass = "http://10.0.20.5:80";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
+        };
+      };
       virtualHosts."_" = {
         default = true;
         locations."/" = {
